0247f68799077d1f0b6603d2023a4384e048086b
mikegachunji <mikegachunji>
2018-06-26 19:30:41 +0300

    Created static files and added bootsrap

--- ujamaa/ujamaa.py
+++ ujamaa/ujamaa.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Michael Gachunji',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'June 26 2018'
+	},
+		{
+		'author': 'Nahashon Njenga',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'June 27 2018'
+	}
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Home Page</h1>"
+	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html', title='About')
 
 
 
