bf2efa7eb71c0a7392fda8541049255a752f678c
mikegachunji <mikegachunji>
2018-07-02 09:24:40 +0300

    Changed from python3.5 to python3.6

--- ujamaa/ujamaa.py
+++ ujamaa/ujamaa.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
 posts = [
 	{
 		'author': 'Michael Gachunji',
