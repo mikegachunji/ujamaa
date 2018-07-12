737917dfecd844f833cab04ce39e770319323b08
mikegachunji <mikegachunji>
2018-07-13 01:21:31 +0300

    Created blueprints and new configurations for restructuring the app

--- ujamaa/ujamaa_models/__init__.py
+++ ujamaa/ujamaa_models/__init__.py
@@ -4,21 +4,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from ujamaa_models.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a58e4aa584b5a356c5d25c8cc28c8df5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info' 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mikegachunji@gmail.com'
-app.config['MAIL_PASSWORD'] = 'maguyz94'
-mail = Mail(app)
+mail = Mail()
 
-from ujamaa_models import routes 
+
+
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
+
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+	from ujamaa_models.users.routes import users 
+	from ujamaa_models.posts.routes import posts
+	from ujamaa_models.main.routes import main
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main) 
+
+	return app
+
